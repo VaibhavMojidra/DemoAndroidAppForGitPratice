98082359e9768f090e4d5dc55566e35be1679c8f
VaibhavMojidra <vmojidra25@gmail.com>
2024-03-09 15:17:49 +0530

    Initial commit

---
+++ app/src/main/java/com/vaibhavmojidra/demoandroidappforgitpratice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vaibhavmojidra.demoandroidappforgitpratice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
